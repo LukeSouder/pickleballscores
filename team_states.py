14d18ed0c512df50e236a72be2c8c5f1e05d839f
Lucas Souder <Lucas@souderfamily.net>
2025-12-31 20:29:21 -0500

    This is the logic for the states to keep score

---
+++ team_states.py
@@ -0,0 +1,52 @@
+
+class Team1Server1:
+    def __init__(self, team1: int, team2: int):
+        self.team1 = team1
+        self.team2 = team2
+        self.team = 1
+        self.server = 1
+
+    def team1_scored(self):
+        return Team1Server1(self.team1 + 1, self.team2)
+    
+    def team2_scored(self):
+        return Team1Server2(self.team1, self.team2)
+
+class Team1Server2:
+    def __init__(self, team1: int, team2: int):
+        self.team1 = team1
+        self.team2 = team2
+        self.team = 1
+        self.server = 2
+
+    def team1_scored(self):
+        return Team1Server2(self.team1 + 1, self.team2)
+    
+    def team2_scored(self):
+        return Team2Server1(self.team1, self.team2)
+    
+class Team2Server1:
+    def __init__(self, team1: int, team2: int):
+        self.team1 = team1
+        self.team2 = team2
+        self.team = 2
+        self.server = 1
+
+    def team1_scored(self):
+        return Team2Server2(self.team1, self.team2)
+    
+    def team2_scored(self):
+        return Team2Server1(self.team1, self.team2 + 1)
+    
+class Team2Server2:
+    def __init__(self, team1: int, team2: int):
+        self.team1 = team1
+        self.team2 = team2
+        self.team = 2
+        self.server = 2
+    def team1_scored(self):
+        return Team1Server1(self.team1, self.team2)
+    
+    def team2_scored(self):
+        return Team2Server2(self.team1, self.team2 + 1)
+    
