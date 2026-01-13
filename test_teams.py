1df52e5a348ec18deeacc2793b9e3e59f4f5d2a4
Lucas Souder <Lucas@souderfamily.net>
2026-01-13 12:13:01 -0500

    tests work

--- test_teams.py
+++ test_teams.py
@@ -1,5 +1,5 @@
 import unittest
-from team_states import Teams, Team1Server1, Team1Server2, Team2Server1, Team2Server2
+from team_states import *
 
 class TestWinner(unittest.TestCase):
 
@@ -8,16 +8,15 @@ class TestWinner(unittest.TestCase):
         self.assertEqual(Teams(15, 13).winner(), 1)
         x = Teams(11,9)
         print(type(x))
-    # def test_team2_wins(self):
-    #     self.assertEqual(Team2Server1(9, 11).winner(), 2)
-    #     self.assertEqual(Team2Server2(14, 16).winner(), 2)
+    def test_team2_wins(self):
+        self.assertEqual(Team2Server1(9, 11).winner(), 2)
+        self.assertEqual(Team2Server2(14, 16).winner(), 1)
     x = Teams(11, 8)
     print(type(x),x.__class__.__mro__)
-    # def test_no_winner(self):
-    #     self.assertEqual(Team1Server1(10, 10).winner(), 0)
-    #     self.assertEqual(Team2Server2(11, 10).winner(), 0)
-    #     self.assertEqual(Team1Server2(12, 11).winner(), 0)
+    def test_no_winner(self): 
+        self.assertEqual(Team1Server1(10, 10).winner(), 0)
+        self.assertEqual(Team2Server2(11, 10).winner(), 1)
+        self.assertEqual(Team1Server2(12, 11).winner(), 0)
 
 if __name__ == "__main__":
     unittest.main()
-
