dd2b1d053d6bfd26763843a36b4757b0aaf51c3a
Lucas Souder <Lucas@souderfamily.net>
2026-01-14 12:26:16 -0500

    undo function added

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import tkinter as tk
 from team_states import *
 
 # Global variables
+state_stack = []
 current_state = Team1Server2(0,0)
+
+def undo():
+    global current_state
+
+    if state_stack:             
+        current_state = state_stack.pop()
+        update_label(label)
+
 
 def update_label(label):
     if current_state.winner() == 1:
@@ -15,15 +24,18 @@ def update_label(label):
 def team1_button():
     global  current_state
     
+    state_stack.append(current_state)
     current_state = current_state.team1_scored()
     update_label(label)
-        #Reset_game()
+     
 
 def team2_button():
     global current_state
+
+    state_stack.append(current_state)
     current_state = current_state.team2_scored()
     update_label(label)
-        #Reset_game()
+       
 
 def reset_game():
     global current_state
@@ -44,6 +56,10 @@ button1.pack(pady=5)
 
 button2 = tk.Button(window, text="Team2", command=team2_button, width=20)
 button2.pack(pady=5)
+
+#undo button
+undo_button = tk.Button(window, text="Undo", command=undo, width=20)
+undo_button.pack(pady=5)
 
 #reset game button
 reset_button = tk.Button(window, text = "Reset Game", command=reset_game, width=20)
